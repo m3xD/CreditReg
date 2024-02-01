bd86e4ab03489f9553156c23eadcc4de93ad4dee
gogo <thaoproduction123@gmail.com>
2024-02-01 11:35:35 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"gorm.io/driver/postgres"
+	_ "gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	_ "gorm.io/gorm"
+	"log"
+)
+
+type MonHoc struct {
+	ID    int
+	tenMH string
+}
+
+type DangKi struct {
+	ID   int
+	idSV int
+	idMH int
+}
+
+type SinhVien struct {
+	ID    int
+	ten   string
+	idLop int
+}
+
+type Lop struct {
+	ID     int
+	tenLop string
+}
+
+func main() {
+	dsn := "host=postgresvm.postgres.database.azure.com user=mexd password=Saccar2108@ dbname=postgres port=5432 sslmode=disable"
+	_, err := gorm.Open(postgres.New(postgres.Config{
+		DSN:                  dsn,
+		PreferSimpleProtocol: true,
+	}), &gorm.Config{})
+	if err != nil {
+		log.Fatalln("Something went wrong")
+	}
+}
