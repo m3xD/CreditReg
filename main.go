d0cddaf7373f3f0a93815c143c85ec945877ba30
duykhanh <thaoproduction123@gmail.com>
2024-02-25 18:32:09 +0700

    finish project

--- main.go
+++ main.go
@@ -1,33 +1,61 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
 	"gorm.io/driver/postgres"
-	_ "gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	_ "gorm.io/gorm"
-	"log"
+	"gorm.io/gorm/logger"
 )
 
+func (mh *MonHoc) TableName() string {
+	return "monhoc"
+}
+
+func (dk *DangKi) TableName() string {
+	return "dangki"
+}
+
+func (sv *SinhVien) TableName() string {
+	return "sinhvien"
+}
+
+func (lop *Lop) TableName() string {
+	return "lop"
+}
+
 type MonHoc struct {
-	ID    int
-	tenMH string
+	ID       int `gorm:"primaryKey"`
+	TenMH    string
+	SinhVien []SinhVien `gorm:"many2many:dangki"`
 }
 
 type DangKi struct {
-	ID   int
-	idSV int
-	idMH int
+	ID         int `gorm:"primaryKey"`
+	SinhVienID int `gorm:"primaryKey"`
+	MonHocID   int
+	MonHoc     MonHoc
+	SinhVien   SinhVien
 }
 
 type SinhVien struct {
-	ID    int
-	ten   string
-	idLop int
+	ID     int      `gorm:"primaryKey"`
+	TenSV  string   `gorm:"column:ten_sv"`
+	LopID  int      `gorm:"column:lop_id"`
+	MonHoc []MonHoc `gorm:"many2many:dangki"`
 }
 
 type Lop struct {
-	ID     int
-	tenLop string
+	ID       uint `gorm:"primaryKey"`
+	TenLop   string
+	SinhVien []SinhVien
+}
+
+type LopSVReg struct {
+	TenSV  string
+	TenLop string
 }
 
 func main() {
@@ -35,8 +63,27 @@ func main() {
 	db, err := gorm.Open(postgres.New(postgres.Config{
 		DSN:                  dsn,
 		PreferSimpleProtocol: true,
-	}), &gorm.Config{})
+	}), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
 	if err != nil {
 		log.Fatalln("Something went wrong")
+	}
+
+	// Hiển thị lớp sinh viên đang học
+	var result []LopSVReg
+	db.Table("sinhvien").Select("sinhvien.ten_sv, lop.ten_lop").Joins("JOIN lop ON sinhvien.lop_id = lop.id").Scan(&result)
+	for _, r := range result {
+		fmt.Printf("%+v\n", r)
+	}
+
+	// Hiển thị các môn sinh viên đã đăng kí
+	var s []SinhVien
+	db.Preload("MonHoc").Find(&s)
+	for _, mh := range s {
+		fmt.Printf("Tên sinh viên: %s\nCác môn đã đăng kí:\n", mh.TenSV)
+		for _, r := range mh.MonHoc {
+			fmt.Printf("%s\n", r.TenMH)
+		}
 	}
 }
